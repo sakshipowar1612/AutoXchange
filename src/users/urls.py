1c6682621994f8f4778fd55ed32d757132f43ea1
sakshipowar1612 <sakshipowar144@gmail.com>
2024-08-25 06:15:23 +0530

    video 59 and 60 register page

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from .views import login_view
-from .views import register_view
+from .views import RegisterView
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
+    path('register/', RegisterView.as_view() , name='register'),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-
+from django.views import View
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
@@ -26,7 +26,22 @@ def login_view(request):
     return  render(request, 'views/login.html', {'login_form':login_form})
 
 
-def register_view(request):
-    register_form = UserCreationForm()
-    return render(request, 'views/register.html', {'register_form': register_form})
+# def register_view(request):
+#     register_form = UserCreationForm()
+#     return render(request, 'views/register.html', {'register_form': register_form})
 
+class RegisterView(View):
+    def get(self, request):
+        register_form = UserCreationForm()
+        return render(request, 'views/register.html', {'register_form': register_form})
+    def post(self, request):
+        register_form = UserCreationForm(data=request.POST)
+        if register_form.is_valid():
+            user = register_form.save()
+            user.refresh_from_db()
+            login(request, user)
+            messages.success(request, f'User registerd successfully.')
+            return redirect('home')
+        else:
+            messages.error(request, f'An error occured trying to register')
+            return render(request, 'views/register.html', {'register_form': register_form})
