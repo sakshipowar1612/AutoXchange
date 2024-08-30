76d2591b4988030e0145e04540f2de9aefd88a88
sakshipowar1612 <sakshipowar144@gmail.com>
2024-08-30 23:23:34 +0530

    lec 91: Django Enviroment Variables

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -10,21 +10,28 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
 from django.contrib import messages
 
+#Initialize Environ
+env = environ.Env()
+env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tghneehthnsjd&$$v_pvmyw(1c-ce-n*e=scn@p&y6jifm+=rj'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(env('DEBUG'))
 
 ALLOWED_HOSTS = ['*'] #IP addresses
 
